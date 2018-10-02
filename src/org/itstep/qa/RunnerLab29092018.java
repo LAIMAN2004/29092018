0c68231e00c139851f232024df8247b34f284c37
Laiman <Laiman2004@gmail.com>
2018-10-02 18:30:00 +0300

    Initial commit

---
+++ src/org/itstep/qa/RunnerLab29092018.java
@@ -0,0 +1,9 @@
+package org.itstep.qa;
+
+public class RunnerLab29092018 {
+    public static void main(String[] args){
+        Lab29092018 lab29092018 = new Lab29092018(1, 2, 3 );
+        Lab29092018 lab290920181Tow = new Lab29092018(4);
+        Lab29092018 lab290920181Try = new Lab29092018();
+    }
+}
